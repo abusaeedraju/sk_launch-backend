348fd8e92ab37d790b672d8b6212f17b70007bd4
unknown <mahmudhasan.hb@gmail.com>
2025-06-30 14:15:29 +0600

    working on user system

--- src/app/route/route.ts
+++ src/app/route/route.ts
@@ -1,38 +1,46 @@
-import { Router } from "express"
-import { userRoutes } from "../modules/user/user.routes"
-import { authRoutes } from "../modules/auth/auth.routes"
-import { jobRoutes } from "../modules/job/job.routes"
-import { postRoutes } from "../modules/post/post.route"
-import { favoriteRoutes } from "../modules/favorite/favorite.route"
-import { applicationRoutes } from "../modules/application/application.route"
+import { Router } from "express";
+import { userRoutes } from "../modules/user/user.routes";
+import { authRoutes } from "../modules/auth/auth.routes";
+import { jobRoutes } from "../modules/job/job.routes";
+import { postRoutes } from "../modules/post/post.route";
+import { favoriteRoutes } from "../modules/favorite/favorite.route";
+import { applicationRoutes } from "../modules/application/application.route";
 
-const router = Router()
+const router = Router();
 const routes = [
-    {
-        path: "/user",
-        component: userRoutes
-    },
-    {
-        path: "/auth",
-        component: authRoutes
-    },
-    {
-        path: "/job",
-        component: jobRoutes
-    },
-    {
-        path: "/post",
-        component: postRoutes
-    },
-    {
-        path: "/favorite",
-        component: favoriteRoutes
-    },
-    {
-        path: "/application",
-        component: applicationRoutes
-    }
-]
+  {
+    path: "/user",
+    component: userRoutes,
+  },
+  {
+    path: "/auth",
+    component: authRoutes,
+  },
+  {
+    path: "/job",
+    component: jobRoutes,
+  },
+  {
+    path: "/post",
+    component: postRoutes,
+  },
+  {
+    path: "/favorite",
+    component: favoriteRoutes,
+  },
+  {
+    path: "/application",
+    component: applicationRoutes,
+  },
+  {
+    path: "/experience",
+    component: applicationRoutes,
+  },
+  {
+    path: "/education",
+    component: applicationRoutes,
+  },
+];
 
-routes.forEach(route => router.use(route.path, route.component))
-export default router 
+routes.forEach((route) => router.use(route.path, route.component));
+export default router;
